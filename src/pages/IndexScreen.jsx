cbf1bc904ff6cb37319d80df20be8e9ff95d56e3
zeta-dev <nvreparaciones@gmail.com>
2024-08-19 23:33:18 -0300

    sigo haciendo pruebas de estilo

--- src/pages/IndexScreen.jsx
+++ src/pages/IndexScreen.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../css/IndexScreen.css';
 import adminImage from '../images/admin.jpg';
-import kitchenImage from '../images/kitchen.webp';
+import kitchenImage from '../images/kitchen3.jpg';
 import waiterImage from '../images/waiter.jpg';
 import clientImage from '../images/client.jpg';
 
 const IndexScreen = () => {
   return (
     <div className="container-fluid d-flex flex-column justify-content-between" style={{ height: 'calc(100vh - 60px)' }}>
-      <div className="row flex-grow-1">
+      <div className="row flex-grow-1 sm-pantalla">
         <div className="col-lg-3 col-md-6 col-12 m-0 p-0 border-0">
           <div className="card h-100 border-0 custom-card">
             <img src={adminImage} className="card-img-top" alt="Admin" />
