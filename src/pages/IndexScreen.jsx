5e968cc6240b38df618abd0cbb539e1f4090cbdb
zeta-dev <nvreparaciones@gmail.com>
2024-08-21 16:43:40 -0300

    se corrigen estilos, quedan pendientes corregir tablets

--- src/pages/IndexScreen.jsx
+++ src/pages/IndexScreen.jsx
@@ -1,40 +1,110 @@
-import React from 'react';
-import '../css/IndexScreen.css';
-import adminImage from '../images/admin.jpg';
-import kitchenImage from '../images/kitchen3.jpg';
-import waiterImage from '../images/waiter.jpg';
-import clientImage from '../images/client.jpg';
+import React from "react";
+import "../css/IndexScreen.css";
+import adminImage from "../images/admin.jpg";
+import kitchenImage from "../images/kitchen3.jpg";
+import waiterImage from "../images/waiter.jpg";
+import clientImage from "../images/client.jpg";
 
 const IndexScreen = () => {
+  let userRole = localStorage.getItem("userRole");
+
   return (
-    <div className="container-fluid d-flex flex-column justify-content-between" style={{ height: 'calc(100vh - 60px)' }}>
+    <div
+      className="container-fluid d-flex flex-column justify-content-between"
+      style={{ height: "calc(100vh - 60px)" }}
+    >
       <div className="row flex-grow-1 sm-pantalla">
-        <div className="col-lg-3 col-md-6 col-12 m-0 p-0 border-0">
-          <div className="card h-100 border-0 custom-card">
+        {/* Admin Panel - Visible only to Admin */}
+        <div
+          className={`col-lg-3 col-md-6 col-12 m-0 p-0 border-0 ${
+            userRole !== "admin" ? "d-none" : ""
+          }`}
+        >
+          <div
+            className={`card h-100 border-0 custom-card ${
+              userRole === "admin" ? "admin-card" : ""
+            }`}
+          >
             <img src={adminImage} className="card-img-top" alt="Admin" />
             <div className="custom-overlay">
               <h5 className="card-title m-0">PANEL ADMIN</h5>
             </div>
           </div>
         </div>
-        <div className="col-lg-3 col-md-6 col-12 m-0 p-0 border-0">
-          <div className="card h-100 border-0 custom-card">
+
+        {/* Kitchen Panel - Visible to Admin and Kitchen */}
+        <div
+          className={`${
+            userRole === "waiter" || userRole === "client"
+              ? "d-none"
+              : userRole === "kitchen"
+              ? "col-lg-12 col-md-12 col-12"
+              : "col-lg-3 col-md-6 col-12"
+          } m-0 p-0 border-0 custom-card`}
+        >
+          <div
+            className={`card h-100 border-0 custom-card ${
+              userRole === "admin" ? "admin-card" : "kitchen-card"
+            }`}
+          >
             <img src={kitchenImage} className="card-img-top" alt="Kitchen" />
             <div className="custom-overlay">
               <h5 className="card-title m-0">PANEL DE COCINA</h5>
             </div>
           </div>
         </div>
-        <div className="col-lg-3 col-md-6 col-12 m-0 p-0 border-0">
-          <div className="card h-100 border-0 custom-card">
+
+        {/* Waiter Panel - Visible to Admin, Kitchen, and Waiter */}
+        <div
+          className={`${
+            userRole === "client"
+              ? "d-none"
+              : userRole === "waiter"
+              ? "col-lg-12 col-md-12 col-12"
+              : userRole === "kitchen"
+              ? "col-lg-12 col-md-12 col-12"
+              : "col-lg-3 col-md-6 col-12"
+          } m-0 p-0 border-0 custom-card`}
+        >
+          <div
+            className={`card h-100 border-0 ${
+              userRole === "admin"
+                ? "admin-card"
+                : userRole === "waiter"
+                ? "waiter-card"
+                : "kitchen-card"
+            }`}
+          >
             <img src={waiterImage} className="card-img-top" alt="Waiter" />
             <div className="custom-overlay">
               <h5 className="card-title m-0">PANEL DE MOZOS</h5>
             </div>
           </div>
         </div>
-        <div className="col-lg-3 col-md-6 col-12 m-0 p-0 border-0">
-          <div className="card h-100 border-0 custom-card">
+
+        {/* Client Panel - Visible to All */}
+        <div
+          className={`${
+            userRole === "client"
+              ? "col-lg-12 col-md-12 col-12"
+              : userRole === "kitchen"
+              ? "col-lg-12 col-md-12 col-12"
+              : userRole === "waiter"
+              ? "col-lg-12 col-md-12 col-12"
+              : "col-lg-3 col-md-6 col-12"
+          } m-0 p-0 border-0 custom-card`}
+        >
+          <div
+            className={`card h-100 border-0 ${
+              userRole === "admin"
+                ? "admin-card"
+                : userRole === "waiter"
+                ? "waiter-card"
+                : userRole === "kitchen"
+                ? "kitchen-card"
+                : "client-card"
+            }`}
+          >
             <img src={clientImage} className="card-img-top" alt="Client" />
             <div className="custom-overlay">
               <h5 className="card-title m-0">CARTA O MENU</h5>
