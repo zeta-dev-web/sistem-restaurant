ae4f70eb6164599bbd6226cd94320c40b3340aaf
zeta-dev <nvreparaciones@gmail.com>
2024-08-23 01:18:03 -0300

    pruebo cambios de estilo al desplazarme entre las card

--- src/pages/IndexScreen.jsx
+++ src/pages/IndexScreen.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/IndexScreen.css";
 import adminImage from "../images/admin.jpg";
 import kitchenImage from "../images/kitchen3.jpg";
@@ -6,17 +6,19 @@ import waiterImage from "../images/waiter.jpg";
 import clientImage from "../images/client.jpg";
 
 const IndexScreen = () => {
-  let userRole = localStorage.getItem("userRole");
+  const [activeCard, setActiveCard] = useState(null);
+  const userRole = localStorage.getItem("userRole");
+
+  const handleCardClick = (role) => {
+    setActiveCard(role);
+  };
 
   return (
     <div
       className="container-fluid d-flex flex-column justify-content-between"
       style={{ height: "calc(100vh - 60px)" }}
     >
-      <div className= {`${
-            userRole === "client" ? "h-100" : "row flex-grow-1"
-          }`}>
-        {/* Admin Panel - Visible only to Admin */}
+      <div className={`${userRole === "client" ? "h-100" : "row flex-grow-1"}`}>
         <div
           className={`col-lg-3 col-md-6 col-12 m-0 p-0 border-0 ${
             userRole !== "admin" ? "d-none" : ""
@@ -25,7 +27,8 @@ const IndexScreen = () => {
           <div
             className={`card h-100 border-0 custom-card ${
               userRole === "admin" ? "admin-card" : ""
-            }`}
+            } ${activeCard && activeCard !== "admin" ? "inactive" : ""}`}
+            onClick={() => handleCardClick("admin")}
           >
             <img src={adminImage} className="card-img-top" alt="Admin" />
             <div className="custom-overlay">
@@ -34,7 +37,6 @@ const IndexScreen = () => {
           </div>
         </div>
 
-        {/* Kitchen Panel - Visible to Admin and Kitchen */}
         <div
           className={`${
             userRole === "waiter" || userRole === "client"
@@ -42,7 +44,10 @@ const IndexScreen = () => {
               : userRole === "kitchen"
               ? "col-lg-4 col-md-12 col-12"
               : "col-lg-3 col-md-6 col-12"
-          } m-0 p-0 border-0 custom-card`}
+          } m-0 p-0 border-0 custom-card ${
+            activeCard && activeCard !== "kitchen" ? "inactive" : ""
+          }`}
+          onClick={() => handleCardClick("kitchen")}
         >
           <div
             className={`card h-100 border-0 custom-card ${
@@ -55,8 +60,6 @@ const IndexScreen = () => {
             </div>
           </div>
         </div>
-
-        {/* Waiter Panel - Visible to Admin, Kitchen, and Waiter */}
         <div
           className={`${
             userRole === "client"
@@ -66,7 +69,10 @@ const IndexScreen = () => {
               : userRole === "kitchen"
               ? "col-lg-4 col-md-12 col-12"
               : "col-lg-3 col-md-6 col-12"
-          } m-0 p-0 border-0 custom-card`}
+          } m-0 p-0 border-0 custom-card ${
+            activeCard && activeCard !== "waiter" ? "inactive" : ""
+          }`}
+          onClick={() => handleCardClick("waiter")}
         >
           <div
             className={`card h-100 border-0 ${
@@ -84,7 +90,6 @@ const IndexScreen = () => {
           </div>
         </div>
 
-        {/* Client Panel - Visible to All */}
         <div
           className={`${
             userRole === "client"
@@ -94,9 +99,12 @@ const IndexScreen = () => {
               : userRole === "waiter"
               ? "col-lg-6 col-md-12 col-12"
               : "col-lg-3 col-md-6 col-12"
-          } m-0 p-0 border-0 custom-card`}
+          } m-0 p-0 border-0 custom-card ${
+            activeCard && activeCard !== "client" ? "inactive" : ""
+          }`}
+          onClick={() => handleCardClick("client")}
         >
-          <div
+         <div
             className={`card h-100 border-0 ${
               userRole === "admin"
                 ? "admin-card"
